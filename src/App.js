b0a29f69ab57074dde40da803b2d3cb84bbebfd2
Kevin Buckley <kevin.buckley2013@gmail.com>
2018-03-16 11:56:16 -0400

    made the initial front-end

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <nav>
+        <div class="nav-wrapper">
+          <h1 class="brand-logo center">Recipe App</h1>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="/recipes">Home</a>
+            </li>
+            <li>
+              <a href="/recipes/new">New Recipe</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
